70ea694c9166059fba3eaaeeb45a3740c62f4154
realfarhanshaukat <farhanshaukat@codistan.org>
2024-10-06 17:09:43 +0500

    Initial commit of portfolio project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { FaLinkedin, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+
+import logo from "../assets/farhan-logo.webp";
+const Navbar = () => {
+  return (
+    <nav className="flex item-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img src={logo} className="mx-2" width={50} height={33} />
+        </a>
+      </div>
+
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/muhammad-farhan-shaukat-44398b243/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+
+        <a
+          href="https://www.github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaTwitter />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,94 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { FaNode, FaWordpress } from "react-icons/fa";
+import { SiFirebase, SiMongodb, SiJavascript } from "react-icons/si";
+
+import { motion } from "framer-motion";
+const iconVariants = (duration) => ({
+  initial: {
+    y: -10,
+  },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+const Technologies = () => {
+  return (
+    <div className="pb-24">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+          className="p-4"
+        >
+          <FaNode className="text-7xl text-green-500" />
+        </motion.div>
+
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+          className="p-4"
+        >
+          <SiMongodb className="text-6xl text-green-700" />
+        </motion.div>
+
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2)}
+          className="p-4"
+        >
+          <SiFirebase className="text-6xl text-orange-500" />
+        </motion.div>
+
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(6)}
+          className="p-4"
+        >
+          <FaWordpress className="text-6xl text-blue-600" />
+        </motion.div>
+
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(4)}
+          className="p-4"
+        >
+          <SiJavascript className="text-6xl text-yellow-500" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technologies;
